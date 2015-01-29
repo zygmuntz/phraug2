1a9f6a5daf4693172d4cd0b3ffc6b5bb9845cd84
Zygmunt Zając <zajac.zygmunt@gmail.com>
2015-01-29 17:52:06 +0100

    verbose output (line counts) for libsvm2vw.py

--- libsvm2vw.py
+++ libsvm2vw.py
@@ -9,12 +9,15 @@ parser.add_argument( "input_file", help = "path to csv input file" )
 parser.add_argument( "output_file", help = "path to output file" )
 parser.add_argument( "-z", "--convert_zeros", help = "convert labels for binary classification from 0 to -1", 
 	action = 'store_true', default = False )
+parser.add_argument("-v", "--verbose", help = "will write line counts to standard out",
+					action = "store_true", default = False )
 
 args = parser.parse_args()
 
 i = open( args.input_file )
 o = open( args.output_file, 'wb' )
 
+count = 0
 for line in i:
 	try:
 		y, x = line.split( " ", 1 )
@@ -29,5 +32,10 @@ for line in i:
 		
 	new_line = y + " |n " + x
 	o.write( new_line )
+	
+	count += 1
+	if args.verbose and count % 100000 == 0:
+		print count
+		
 
 
